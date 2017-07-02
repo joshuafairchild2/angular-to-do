f5b19cb2da5a93a84e6d8c163fe5979eb856c7fb
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-02 11:58:17 -0700

    add logic to the root component so that it now displays (relatively) dynamic content

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,22 @@ import {Component} from '@angular/core';
   selector: 'app-root',
   template: `
   <div class='container'>
-    <h1> My First Angular 2 App</h1>
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>Current focus: {{currentFocus}}</h3>
+    <ul>
+      <li>{{firstTask.description}}</li>
+    </ul>
   </div>
   `
 })
 
 export class AppComponent {
-
+  currentFocus: string = 'Angular';
+  today = new Date();
+  month = this.today.getMonth() + 1;
+  day = this.today.getDate();
+  year = this.today.getFullYear();
+  firstTask = {
+    description: 'Finish angular work'
+  }
 }
